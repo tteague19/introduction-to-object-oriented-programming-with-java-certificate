f6baf6833fe310af8532270e06751b97553abd6f
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-21 11:29:31 -0400

    Modified the date input section
    I changed the code so that a user only provides a day of the week
    instead of a longer date.

--- course-01-foundations-and-syntax-basics/module-02/FahrenheitToCelsius.java
+++ course-01-foundations-and-syntax-basics/module-02/FahrenheitToCelsius.java
@@ -10,8 +10,8 @@ public class FahrenheitToCelsius {
         // the input that follows.
         input.nextLine();
 
-        System.out.print("Enter the day of the week along with the month, day, and year: ");
-        String day = input.nextLine();
+        System.out.print("Enter a day of the week and press \"Enter\": ");
+        String day = input.next();
 
         double celsius = (5D / 9) * (fahrenheit - 32);
         System.out.println(day + " Fahrenheit: " + fahrenheit);
