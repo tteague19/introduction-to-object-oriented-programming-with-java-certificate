8ab01a08e749ed25f3111b183a600b2faf04258d
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-21 09:20:58 -0400

    Corrected a misspelling

--- course-01-foundations-and-syntax-basics/module-1/FahrenheitToCelsius.java
+++ course-01-foundations-and-syntax-basics/module-1/FahrenheitToCelsius.java
@@ -4,12 +4,12 @@ The values represent two weekend days
 */
 public class FahrenheitToCelsius {
     public static void main(String[] args) {
-        int saturdayFarenheit;
-        int sundayFarenheit;
-        saturdayFarenheit = 78;
-        sundayFarenheit = 81;
-        double saturdayCelsius = (5.0 / 9) * (saturdayFarenheit - 32);
-        double sundayCelsius = (5.0 / 9) * (sundayFarenheit - 32);
+        int saturdayFahrenheit;
+        int sundayFahrenheit;
+        saturdayFahrenheit = 78;
+        sundayFahrenheit = 81;
+        double saturdayCelsius = (5.0 / 9) * (saturdayFahrenheit - 32);
+        double sundayCelsius = (5.0 / 9) * (sundayFahrenheit - 32);
         System.out.println("Weekend Averages");
         System.out.println("Saturday: " + saturdayCelsius);
         System.out.println("Sunday: " + sundayCelsius);
