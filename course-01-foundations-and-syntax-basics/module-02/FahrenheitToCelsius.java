3d3037e305114de65eb5319aad724be64327fd0f
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-21 11:06:50 -0400

    Added date information
    I added the functionality to enter a date that we will display in
    addition to the temperature conversion. Note the handling of the input
    stream in line 11. Since nextInt() leaves a trailing new line character,
    we must absorb it with a call to nextLine() to be able to read the date
    information from the user.

--- course-01-foundations-and-syntax-basics/module-02/FahrenheitToCelsius.java
+++ course-01-foundations-and-syntax-basics/module-02/FahrenheitToCelsius.java
@@ -5,7 +5,16 @@ public class FahrenheitToCelsius {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a Fahrenheit value and press \"Enter\": ");
         int fahrenheit = input.nextInt();
+
+        // We must "clean up" the trailing new line before processing
+        // the input that follows.
+        input.nextLine();
+
+        System.out.print("Enter the day of the week along with the month, day, and year: ");
+        String day = input.nextLine();
+
         double celsius = (5D / 9) * (fahrenheit - 32);
-        System.out.println("Celcius: " + celsius);
+        System.out.println(day + " Fahrenheit: " + fahrenheit);
+        System.out.println(day + " Celcius: " + celsius);
     }
 }
