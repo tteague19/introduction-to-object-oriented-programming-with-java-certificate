3e6cd9a0e671fa9ee5fbea5cb50f9f4033e8d1f2
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-21 12:10:00 -0400

    Completed Section 1, Task 1 of HW1
    I completed the instructions given in the first sub-task of Section 1
    HW1. Note that I provide the text of the sub-task above the code the
    implements it.

---
+++ course-01-foundations-and-syntax-basics/module-02/HW-1/PrimitiveOperations.java
@@ -0,0 +1,17 @@
+public class PrimitiveOperations {
+    public static void main(String[] args) {
+        // First, declare and initialize two variables, an integer
+        // type (byte, short, int, or long) and a floating point value
+        // (float or double). The names and values can be whatever you
+        // like, for this step and all others. Make sure that the
+        // numbers you choose can be stored within the respective
+        // primitive type you choose. Print each of these values out
+        // on their own line using System.out.println().
+        int intVar;
+        double doubleVar;
+        intVar = 19;
+        doubleVar = 1729.56;
+        System.out.println(intVar);
+        System.out.println(doubleVar);
+    }
+}
