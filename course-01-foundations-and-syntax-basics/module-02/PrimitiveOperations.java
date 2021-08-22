e63acb1f8927e124f4212576752061ba25d0bff4
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-21 20:40:00 -0400

    Added headings
    I added headings above each problem statement to identify which sub-task
    each set of comments corresponds to.

--- course-01-foundations-and-syntax-basics/module-02/PrimitiveOperations.java
+++ course-01-foundations-and-syntax-basics/module-02/PrimitiveOperations.java
@@ -1,5 +1,6 @@
 public class PrimitiveOperations {
     public static void main(String[] args) {
+        // Sub-Task 1
         // First, declare and initialize two variables, an integer
         // type (byte, short, int, or long) and a floating point value
         // (float or double). The names and values can be whatever you
@@ -14,6 +15,7 @@ public class PrimitiveOperations {
         System.out.println(intVar);
         System.out.println(doubleVar);
 
+        // Sub-Task 2
         // Multiply these variables together, and assign the outcome
         // to a new variable, ensuring that no data is lost. For
         // example, if I multiply 5 and 3.5, the answer should be
@@ -21,23 +23,27 @@ public class PrimitiveOperations {
         double product = intVar * doubleVar;
         System.out.println(product);
 
+        // Sub-Task 3
         // Use casting to convert the integer from the first step to a
         // floating point value and store that in another new
         // variable. Print out the value.
         float convertedIntToFloat = (float) intVar;
         System.out.println(convertedIntToFloat);
 
+        // Sub-Task 4
         // Use casting to convert the floating point value from the
         // first step to an integer type and store that in a new
         // variable. Print out the value.
         int convertedDoubleToInt = (int) doubleVar;
         System.out.println(convertedDoubleToInt);
 
+        // Sub-Task 5
         // Shifting focus, declare a char variable, and assign an
         // uppercase letter to it. Print out this value.
         char charVar = 'T';
         System.out.println(charVar);
 
+        // Sub-Task 6
         // Using a numerical operation, change the letter to the same
         // letter, but in lowercase. Use a numerical operation - do
         // not reassign the variable. You may want to review a table
