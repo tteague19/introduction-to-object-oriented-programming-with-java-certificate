a57412bc8b2c48b74ee1bcb6544939ed9ba22058
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-28 10:41:33 -0400

    Added multi-way branching

--- course-01-foundations-and-syntax-basics/module-03/FahrenheitToCelsius.java
+++ course-01-foundations-and-syntax-basics/module-03/FahrenheitToCelsius.java
@@ -2,18 +2,28 @@ import java.util.Scanner;
 
 public class FahrenheitToCelsius {
     public static void main(String[] args) {
+        final int MIN_PARK_TEMP = 70;
+        final int MIN_POOL_TEMP = 85;
+        final int MAX_POOL_TEMP = 90;
+
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a Fahrenheit value: ");
         int fahrenheit = input.nextInt();
         System.out.print("Enter a day of the week: ");
         String day = input.next();
         double celsius = (5D / 9) * (fahrenheit - 32);
+
+        System.out.println(day + " Fahrenheit: " + fahrenheit);
         System.out.printf("%s %.1f \n", day + " Celsius: ", celsius);
 
-        if (fahrenheit > 70) {
-            System.out.println("Yay! Go to the park.");
+        if ((fahrenheit >= MIN_POOL_TEMP) && (fahrenheit <= MAX_POOL_TEMP)) {
+            System.out.println("Go to the swimming pool");
+        } else if ((fahrenheit < MIN_POOL_TEMP) && (fahrenheit >= MIN_PARK_TEMP)) {
+            System.out.println("Go to the park.");
         } else {
-            System.out.println("Stay home where there is Wi-Fi and learn 1331 online.");
+            System.out.println("Stay at home.");
         }
+
+        System.out.println("Bye!");
     }
 }
