f14ab2b37409f25a0ab50e9b7fd6cdad38aaae8e
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-28 11:04:19 -0400

    Added nested if-else statements

--- course-01-foundations-and-syntax-basics/module-03/FahrenheitToCelsius.java
+++ course-01-foundations-and-syntax-basics/module-03/FahrenheitToCelsius.java
@@ -13,15 +13,46 @@ public class FahrenheitToCelsius {
         String day = input.next();
         double celsius = (5D / 9) * (fahrenheit - 32);
 
+        boolean raining;
+        System.out.print("Is it raining? (y/n): ");
+        String rainInput = input.next().toLowerCase();
+        raining = rainInput.startsWith("y") ? true : false;
+
         System.out.println(day + " Fahrenheit: " + fahrenheit);
         System.out.printf("%s %.1f \n", day + " Celsius: ", celsius);
 
+
         if ((fahrenheit >= MIN_POOL_TEMP) && (fahrenheit <= MAX_POOL_TEMP)) {
-            System.out.println("Go to the swimming pool");
+
+            if (raining) {
+                System.out.println("Bummer! The pool is closed due to the rain.");
+            } else {
+                System.out.println("Yay! It's at least "
+                                + MIN_POOL_TEMP
+                                + " degrees but under "
+                                + MAX_POOL_TEMP
+                                + ".");
+                System.out.println("Time to go to the pool.");
+            }
+
         } else if ((fahrenheit < MIN_POOL_TEMP) && (fahrenheit >= MIN_PARK_TEMP)) {
-            System.out.println("Go to the park.");
+
+            if (raining) {
+                System.out.println("If you go to the park, bring an umbrella!");
+            } else {
+                System.out.println("Yay! It's at least "
+                                    + MIN_PARK_TEMP
+                                    + " degrees but under "
+                                    + MIN_POOL_TEMP
+                                    + ".");
+                System.out.println("Time to go to the park.");
+            }
         } else {
-            System.out.println("Stay at home.");
+            if (raining) {
+                System.out.println("Stay inside where it is dry.");
+            } else {
+                System.out.println("Stay inside where it is comfortable.");
+            }
         }
 
         System.out.println("Bye!");
