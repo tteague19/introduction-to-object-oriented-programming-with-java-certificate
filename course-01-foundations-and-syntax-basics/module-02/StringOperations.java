91dd1fef101f56433d76b8d87418e0d40e064bed
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-28 08:18:44 -0400

    Completed Sub-Task 1 of Section 2 of HW1

--- course-01-foundations-and-syntax-basics/module-02/StringOperations.java
+++ course-01-foundations-and-syntax-basics/module-02/StringOperations.java
@@ -5,5 +5,19 @@ public class StringOperations {
         // Print it out.
         String myName = "Thomas";
         System.out.println(myName);
+
+        // Sub-Task 2
+        // Pick the first letter in your name, and replace it with
+        // ‘A’. Then, replace the last letter in your name with ‘Z’.
+        // Print out the result. Recall that, in Java, strings are
+        // immutable, meaning you cannot change a String in-place. Do
+        // NOT just hard-code a new String with the first and last
+        // letters changed
+
+        // Fortunately, my name does not contain any repeated letters.
+        // If it did,
+        String myNameModified = myName.replace('T', 'A');
+        myNameModified = myNameModified.replace('s', 'Z');
+        System.out.println(myNameModified);
     }
 }
