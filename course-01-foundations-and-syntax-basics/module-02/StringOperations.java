2aa40540cb8cfc171d93ba1a1082e65b6cf41d53
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-28 08:38:52 -0400

    Completed Sub-Task 4 of Section 2 of HW1

--- course-01-foundations-and-syntax-basics/module-02/StringOperations.java
+++ course-01-foundations-and-syntax-basics/module-02/StringOperations.java
@@ -27,5 +27,20 @@ public class StringOperations {
         // www.stackoverflow.com. Print out this address.
         String urlString = "www.80000hours.org";
         System.out.println(urlString);
+
+        // Sub-Task 4
+        // This last operation could be a little tricky. Create a
+        // substring of the variable that’s just the “name” section,
+        // and concatenate the integer “1331” to the end. For example,
+        // www.gatech.edu would become gatech1331. Print out this
+        // final result. Note: the String class has a .length() method
+        // which you’ll likely find useful here but is not necessary.
+        int prefixLen = "www.".length();
+        int suffixLen = ".org".length();
+        int urlLen = urlString.length();
+        int endIndex = urlLen - suffixLen;
+        String subString = urlString.substring(prefixLen, endIndex);
+        String concatString = subString.concat("1331");
+        System.out.println(concatString);
     }
 }
