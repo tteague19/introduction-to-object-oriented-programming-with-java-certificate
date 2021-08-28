5efae58e6968a48d9ed736513e35a43bdad4664b
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-28 11:48:37 -0400

    Modified my code to match the grader's expectation

--- course-01-foundations-and-syntax-basics/module-03/Calculator.java
+++ course-01-foundations-and-syntax-basics/module-03/Calculator.java
@@ -5,14 +5,8 @@ public class Calculator {
 
         // Sub-Task 1
         // Print out a list of operations for the user
-        System.out.println("Hello and welcome to the Calculator app!");
-        System.out.println("The operations I can perform are:");
-        System.out.println("add");
-        System.out.println("subtract");
-        System.out.println("multiply");
-        System.out.println("divide");
-        System.out.println("alphabetize");
-        System.out.print("Which would you like me to perform for you? ");
+        System.out.println("List of operations: add subtract multiply divide alphabetize");
+        System.out.println("Enter an operation:");
 
         // Prompt the user to enter an operation. This operation must
         // be processed as case-insensitive.
