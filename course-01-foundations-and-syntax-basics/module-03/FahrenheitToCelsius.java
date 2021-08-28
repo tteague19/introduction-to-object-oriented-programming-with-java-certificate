94088e8af2a58cf15a1d91c9559b9de2f75d8ac3
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-28 09:50:06 -0400

    Added an else-statement

--- course-01-foundations-and-syntax-basics/module-03/FahrenheitToCelsius.java
+++ course-01-foundations-and-syntax-basics/module-03/FahrenheitToCelsius.java
@@ -9,8 +9,11 @@ public class FahrenheitToCelsius {
         String day = input.next();
         double celsius = (5D / 9) * (fahrenheit - 32);
         System.out.printf("%s %.1f \n", day + " Celsius: ", celsius);
+
         if (fahrenheit > 70) {
             System.out.println("Yay! Go to the park.");
+        } else {
+            System.out.println("Stay home where there is Wi-Fi and learn 1331 online.");
         }
     }
 }
