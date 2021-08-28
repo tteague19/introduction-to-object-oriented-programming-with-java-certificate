0823680836ba0b1968fad8796883b1b3284aba38
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-28 11:45:34 -0400

    Completed Sub-Task 2 of HW2

--- course-01-foundations-and-syntax-basics/module-03/Calculator.java
+++ course-01-foundations-and-syntax-basics/module-03/Calculator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
 
@@ -11,5 +13,28 @@ public class Calculator {
         System.out.println("divide");
         System.out.println("alphabetize");
         System.out.print("Which would you like me to perform for you? ");
+
+        // Prompt the user to enter an operation. This operation must
+        // be processed as case-insensitive.
+        // If the user enters an invalid operation, the program should
+        // print the following error message and terminate gracefully.
+        // Invalid input entered. Terminating...
+        Scanner input = new Scanner(System.in);
+        String userInput = input.next().toLowerCase();
+
+        switch (userInput) {
+            case "add":
+                break;
+            case "subtract":
+                break;
+            case "multiply":
+                break;
+            case "divide":
+                break;
+            case "alphabetize":
+                break;
+            default:
+                System.out.println("Invalid input entered. Terminating...");
+        }
     }
 }
