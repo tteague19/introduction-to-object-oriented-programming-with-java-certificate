99a256f5761b25696e13d4c21ff9a1874d8a3fe3
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-28 11:38:42 -0400

    Completed Sub-Task 1 of HW2

---
+++ course-01-foundations-and-syntax-basics/module-03/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        // Sub-Task 1
+        // Print out a list of operations for the user
+        System.out.println("Hello and welcome to the Calculator app!");
+        System.out.println("The operations I can perform are:");
+        System.out.println("add");
+        System.out.println("subtract");
+        System.out.println("multiply");
+        System.out.println("divide");
+        System.out.println("alphabetize");
+        System.out.print("Which would you like me to perform for you? ");
+    }
+}
