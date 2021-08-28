dc6e7c32ea5d447df0cfc4bf357307cc0eb658b9
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-28 08:12:38 -0400

    Completed Sub-Task 1 of Section 2 of HW1

--- course-01-foundations-and-syntax-basics/module-02/StringOperations.java
+++ course-01-foundations-and-syntax-basics/module-02/StringOperations.java
@@ -1,3 +1,9 @@
 public class StringOperations {
-
+    public static void main(String[] args) {
+        // Sub-Task 1
+        // Create a new String object and assign it your name.
+        // Print it out.
+        String myName = "Thomas";
+        System.out.println(myName);
+    }
 }
