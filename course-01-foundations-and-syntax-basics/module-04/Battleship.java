17bcc183bdac6821d5a1204da12b33d574a973e1
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-28 19:28:21 -0400

    Created the Battleship.java file
    Note that the course provided the printBattleShip() method.

---
+++ course-01-foundations-and-syntax-basics/module-04/Battleship.java
@@ -0,0 +1,23 @@
+public class Battleship {
+    public static void main(String[] args) {
+
+    }
+
+    // Use this method to print game boards to the console.
+	private static void printBattleShip(char[][] player) {
+		System.out.print("  ");
+		for (int row = -1; row < 5; row++) {
+			if (row > -1) {
+				System.out.print(row + " ");
+			}
+			for (int column = 0; column < 5; column++) {
+				if (row == -1) {
+					System.out.print(column + " ");
+				} else {
+					System.out.print(player[row][column] + " ");
+				}
+			}
+			System.out.println("");
+		}
+    }
+}
