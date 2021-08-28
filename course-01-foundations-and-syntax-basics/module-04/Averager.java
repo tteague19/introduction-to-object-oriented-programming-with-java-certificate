ef856435d9fb8913db62cb09b3b1e312b44284f0
Thomas Teague <thomas.d.teague1@gmail.com>
2021-08-28 18:37:08 -0400

    Changed for loop to for each loop

--- course-01-foundations-and-syntax-basics/module-04/Averager.java
+++ course-01-foundations-and-syntax-basics/module-04/Averager.java
@@ -2,8 +2,8 @@ public class Averager {
     public static void main(String[] args) {
         double[] weekHighs = {80, 70, 75, 69, 72, 74, 90};
         double highsSum = 0;
-        for (int index = 0; index < weekHighs.length; index++) {
-            highsSum = highsSum + weekHighs[index];
+        for (double dayHigh : weekHighs) {
+            highsSum = highsSum + dayHigh;
         }
         double averageHighs = highsSum / weekHighs.length;
         System.out.println("Average is: " + averageHighs);
