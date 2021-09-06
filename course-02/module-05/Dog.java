61a2bcde76cbc84a3bfcfdd4d99080ee665ead7c
Thomas Teague <thomas.d.teague1@gmail.com>
2021-09-06 10:26:09 -0400

    Added an equals() method to Dog

--- course-02/module-05/Dog.java
+++ course-02/module-05/Dog.java
@@ -15,8 +15,23 @@ public class Dog extends Canine {
 
     public void groom() {};
 
+    public boolean equals(Object o) {
+        if (!(o instanceof Dog)) {
+            return false;
+        }
+
+        Dog dog = (Dog) o;
+        return ((dog.size == this.size) && (dog.name.equals(this.name)));
+    }
+
     public static void main(String[] args) {
         Dog spot = new Dog("Spot", 9.6);
         spot.bark();
+
+        Dog spotTwin = new Dog("Spot", 9.6);
+        System.out.println(spot.equals(spotTwin));
+
+        Dog spotBig = new Dog("Spot", 15.0);
+        System.out.println(spot.equals(spotBig));
     }
 }
