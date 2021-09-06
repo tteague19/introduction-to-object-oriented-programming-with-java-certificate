d4d51f9aa47f9b411ed8a70d5154ceacee909286
Thomas Teague <thomas.d.teague1@gmail.com>
2021-09-06 07:35:30 -0400

    Created a simple class

---
+++ course-02/module-05/Insect.java
@@ -0,0 +1,13 @@
+public class Insect {
+
+    // Instance variables
+    private double weight;
+    private int x;
+    private int y;
+
+    // Test method
+    public static void main(String[] args) {
+        Insect buzz1 = new Insect();
+        Insect buzz2 = new Insect();
+    }
+}
