4010dfe9eeca1bf8802140e0b86862bcbcdd5029
Thomas Teague <thomas.d.teague1@gmail.com>
2021-09-06 09:46:55 -0400

    Implemented a simple class
    I created the Canine class, which we will use to demonstrate
    inheritance.

---
+++ course-02/module-05/Canine.java
@@ -0,0 +1,12 @@
+public class Canine {
+
+    protected double size;
+
+    public Canine(double size) {
+        this.size = size;
+    }
+
+    public void bark() {
+        System.out.println("Woof Woof");
+    }
+}
