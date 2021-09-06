8c7b2367979a8af2ecbc4aa98d356e5f14e2f18c
Thomas Teague <thomas.d.teague1@gmail.com>
2021-09-06 09:59:29 -0400

    Implemented a sub-class of Canine for wolves

---
+++ course-02/module-05/Wolf.java
@@ -0,0 +1,25 @@
+public class Wolf extends Canine {
+
+    protected int rank;
+
+    public Wolf(int rank, double size) {
+        super(size);
+        this.rank = rank;
+    }
+
+    public int getRank() {
+        return this.rank;
+    }
+
+    public void bark() {
+        // Wolves bark three times in our case.
+        for (int i = 1; i <= 3; i++) {
+            super.bark();
+        }
+    }
+
+    public static void main(String[] args) {
+        Wolf alpha = new Wolf(1, 17.1);
+        alpha.bark();
+    }
+}
