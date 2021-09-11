55c922c23a9380bc12aea89e32235ec1d9d516e5
Thomas Teague <thomas.d.teague1@gmail.com>
2021-09-11 11:01:12 -0400

    Created the Crewmate interface

---
+++ course-02/module-06/Crewmate.java
@@ -0,0 +1,12 @@
+// This file defines an interface with the name Crewmate.
+
+// Methods
+// completeTask()
+// Abstract method that does not take in anything and does not
+// return anything
+// (Note: any class that implements Crewmate must provide a method
+// definition for this method)
+
+public interface Crewmate {
+    public void completeTask();
+}
