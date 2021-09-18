bc4dc509c1df2ead4369ab8bc158c5429f784096
Thomas Teague <thomas.d.teague1@gmail.com>
2021-09-18 08:38:01 -0400

    Implemented the driver code for the game

--- course-02/module-06/Gameplay.java
+++ course-02/module-06/Gameplay.java
@@ -74,5 +74,66 @@
 // certain keywords.
 
 public class Gameplay {
+    public static void main(String[] args) {
+        BlueAstronaut bob = new BlueAstronaut("Bob", 20, 6, 30);
+        BlueAstronaut heath = new BlueAstronaut("Heath", 30, 3, 21);
+        BlueAstronaut albert = new BlueAstronaut("Albert", 44, 2, 0);
+        BlueAstronaut angel = new BlueAstronaut("Angel", 0, 1, 0);
 
+        RedAstronaut liam = new RedAstronaut("Liam", 19, "experienced");
+        RedAstronaut suspiciousPerson = new RedAstronaut("Suspicious Person", 100, "expert");
+
+        liam.sabotage(bob);
+        System.out.println(bob);
+
+        liam.freeze(suspiciousPerson);
+        System.out.println(suspiciousPerson);
+
+        liam.freeze(albert);
+        System.out.println(liam);
+        System.out.println(albert);
+
+        albert.emergencyMeeting();
+
+        // Error here
+        suspiciousPerson.emergencyMeeting();
+
+        bob.emergencyMeeting();
+        System.out.println(suspiciousPerson);
+
+        heath.completeTask();
+        System.out.println(heath);
+
+        heath.completeTask();
+        System.out.println(heath);
+
+        heath.completeTask();
+        System.out.println(heath);
+
+        // Errors from here onward
+        liam.freeze(angel);
+        System.out.println(angel);
+        System.out.println(liam);
+
+        liam.sabotage(bob);
+        System.out.println(bob);
+        liam.sabotage(bob);
+        System.out.println(bob);
+
+        liam.freeze(bob);
+        System.out.println(bob);
+
+        // A situation in which the Crewmates win
+        // angel.emergencyMeeting();
+        // System.out.println(liam);
+
+        // A situation in which the Imposters win
+        for (int i = 0; i < 5; i++) {
+            liam.sabotage(heath);
+            System.out.println(heath);
+        }
+
+        liam.freeze(heath);
+        System.out.println(heath);
+    }
 }
