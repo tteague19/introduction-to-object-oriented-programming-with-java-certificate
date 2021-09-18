a004463a8587af2904dcec0062b2cf08e9242dad
Thomas Teague <thomas.d.teague1@gmail.com>
2021-09-18 09:11:56 -0400

    Added a static method to the Groomable interface

--- course-02/module-07/Groomable.java
+++ course-02/module-07/Groomable.java
@@ -4,4 +4,9 @@ public interface Groomable {
     default void pay() {
         System.out.println("Cha-Ching!");
     }
+
+    static String calculateTip(double price, double percentage) {
+        double rawTip = price * (percentage / 100);
+        return String.format("$%,.2f", rawTip);
+    }
 }
