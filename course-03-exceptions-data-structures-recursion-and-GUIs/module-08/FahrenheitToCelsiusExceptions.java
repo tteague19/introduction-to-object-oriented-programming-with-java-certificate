9b6034cffa92f966af3997c7afccf79b320711c6
Thomas Teague <thomas.d.teague1@gmail.com>
2021-09-25 08:05:04 -0400

    Added a finally block and use case for it

--- course-03-exceptions-data-structures-recursion-and-GUIs/module-08/FahrenheitToCelsiusExceptions.java
+++ course-03-exceptions-data-structures-recursion-and-GUIs/module-08/FahrenheitToCelsiusExceptions.java
@@ -14,20 +14,25 @@ public class FahrenheitToCelsiusExceptions {
                 success = true;
                 }
             catch(InputMismatchException e) {
-                // We must clear the input buffer to prepare for the
-                // next iteration of the while loop.
-                input.nextLine();
                 System.out.println("Sorry, that wasn't an int");
                 System.out.println("Please try again.");
             }
             catch(Exception e) {
                 System.out.println(e.getMessage());
             }
+            finally {
+                // We must clear the input buffer to prepare for the
+                // next iteration of the while loop as well as the
+                // step in which we capture a day of the week.
+                input.nextLine();
+            }
         }
 
+        System.out.print("Enter a day of the week: ");
+        String day = input.nextLine();
         double celsius = (5D / 9) * (fahrenheit - 32);
-        System.out.printf("Fahrenheit: %d\n", fahrenheit);
-        System.out.printf("Celsius:  %.1f\n", celsius);
+        System.out.printf("%s Fahrenheit: %d\n", day, fahrenheit);
+        System.out.printf("%s Celsius:  %.1f\n", day, celsius);
 
     }
 }
