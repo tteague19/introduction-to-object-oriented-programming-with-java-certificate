6daedbe0beb309d1a3ca32d0a372167ee741bf4e
Thomas Teague <thomas.d.teague1@gmail.com>
2021-09-25 07:48:44 -0400

    Added an exception-controlled loop

--- course-03-exceptions-data-structures-recursion-and-GUIs/module-08/FahrenheitToCelsiusExceptions.java
+++ course-03-exceptions-data-structures-recursion-and-GUIs/module-08/FahrenheitToCelsiusExceptions.java
@@ -4,16 +4,27 @@ import java.util.InputMismatchException;
 public class FahrenheitToCelsiusExceptions {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a Fahrenheit value: ");
-        try {
-            int fahrenheit = input.nextInt();
-            double celsius = (5D / 9) * (fahrenheit - 32);
-            System.out.printf("Fahrenheit: %d\n", fahrenheit);
-            System.out.printf("Celsius:  %.1f\n", celsius);
+        boolean success = false;
+        int fahrenheit = 0;
+
+        while (!success) {
+            try {
+                System.out.print("Enter a Fahrenheit value: ");
+                fahrenheit = input.nextInt();
+                success = true;
+                }
+            catch(InputMismatchException e) {
+                // We must clear the input buffer to prepare for the
+                // next iteration of the while loop.
+                input.nextLine();
+                System.out.println("Sorry, that wasn't an int");
+                System.out.println("Please try again.");
+            }
         }
-        catch(InputMismatchException e) {
-            System.out.println("Sorry, that wasn't an int");
-            System.out.println("Please re-run the program again.");
-        }
+
+        double celsius = (5D / 9) * (fahrenheit - 32);
+        System.out.printf("Fahrenheit: %d\n", fahrenheit);
+        System.out.printf("Celsius:  %.1f\n", celsius);
+
     }
 }
