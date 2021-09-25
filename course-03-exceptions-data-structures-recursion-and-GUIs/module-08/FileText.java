a0dbfca27cf8b29af14c61a581fdf3c622c0d9bd
Thomas Teague <thomas.d.teague1@gmail.com>
2021-09-25 08:36:09 -0400

    Modified FileText to write to a file

--- course-03-exceptions-data-structures-recursion-and-GUIs/module-08/FileText.java
+++ course-03-exceptions-data-structures-recursion-and-GUIs/module-08/FileText.java
@@ -1,26 +1,32 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FileText {
     public static void main(String[] args) {
         String inputFileName = args[0];
         String word = args[1];
+        String outputFileName = args[2];
 
         File fileIn = new File(inputFileName);
+        File fileOut = new File(outputFileName);
 
         Scanner fileScan = null;
+        PrintWriter filePrint = null;
 
         try {
             fileScan = new Scanner(fileIn);
+            filePrint = new PrintWriter(fileOut);
+
             int lineCount = 0;
-            System.out.printf("Lines in %s containing %s\n",
+            filePrint.printf("Lines in %s containing %s\n",
                                 args[0], args[1]);
 
             while (fileScan.hasNextLine()) {
                 String line = fileScan.nextLine();
                 if (line.contains(word)) {
-                    System.out.println(lineCount + ": " + line);
+                    filePrint.println(lineCount + ": " + line);
                 }
                 lineCount++;
             }
@@ -31,6 +37,9 @@ public class FileText {
         finally {
             if (fileScan != null) {
                 fileScan.close();
+            }
+            if (filePrint != null) {
+                filePrint.close();
             }
         }
     }
