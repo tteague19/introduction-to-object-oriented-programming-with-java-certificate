a560744fb864dd230d5bd84e7538105071989d2d
Thomas Teague <thomas.d.teague1@gmail.com>
2021-10-02 06:29:37 -0400

    Implemented the InvalidPetException class

--- course-03-exceptions-data-structures-recursion-and-GUIs/module-08/InvalidPetException.java
+++ course-03-exceptions-data-structures-recursion-and-GUIs/module-08/InvalidPetException.java
@@ -1,3 +1,15 @@
-public class InvalidPetException {
+// An unchecked exception with two constructors
+//
+// Constructors
+// InvalidPetException() has message “Your pet is invalid!”
+// InvalidPetException(String s) has message s
 
+public class InvalidPetException extends RuntimeException {
+    public InvalidPetException() {
+        super("Your pet is invalid!");
+    }
+
+    public InvalidPetException(String s) {
+        super(s);
+    }
 }
