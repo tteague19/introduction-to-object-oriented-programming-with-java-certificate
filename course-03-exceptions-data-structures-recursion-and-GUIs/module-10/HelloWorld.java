ba9b82cb9fdd83725620ddc3d7e4e5bc022a4302
Thomas Teague <thomas.d.teague1@gmail.com>
2021-10-07 07:18:14 -0400

    Implemented a GUI-based HelloWorld program

---
+++ course-03-exceptions-data-structures-recursion-and-GUIs/module-10/HelloWorld.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
+
+    public void start(Stage mainStage) {
+        mainStage.setTitle("Hello World Program");
+
+        Button btn = new Button();
+        btn.setText("Print Hello World!");
+        btn.setOnAction(new CustomEventHandler());
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 300);
+        mainStage.setScene(scene);
+        mainStage.show();
+    }
+
+    private class CustomEventHandler implements EventHandler<ActionEvent> {
+        public void handle(ActionEvent event) {
+            System.out.println("Hello World!");
+        }
+    }
+}
